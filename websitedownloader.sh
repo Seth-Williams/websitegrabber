932d815c54410816a05a509fe5c797268e36ea0f
Seth-Williams <99839823+Seth-Williams@users.noreply.github.com>
2022-03-25 13:46:20 -0700

    Create websitedownloader.sh

---
+++ websitedownloader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install httrack
+sudo apt install httrack
+
+# create directory for the websites
+mkdir websites
+
+# download hacktricks
+httrack https://book.hacktricks.xyz/ -O websites
+
