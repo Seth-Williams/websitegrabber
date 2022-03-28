72b3dd93f3d3ecf3565744fdde3e99917152cc88
Seth Williams <seth.l.williams@outlook.com>
2022-03-28 10:25:00 -0700

    update script

--- websitedownloader.sh
+++ websitedownloader.sh
@@ -3,13 +3,48 @@
 # install httrack
 sudo apt install httrack
 
-# create directory for the websites
-mkdir ~/websites
+# create directory for the websites if doesn't exist
+DIR=/home/kali/websites
+if [ ! -d "$DIR" ] # check to see if exists
+then # make directory
+  echo "Making dir for websites @ $DIR..."
+  mkdir ~/websites
+else
+  echo "Dir already exists. Moving on... "
+fi
+
+# delete lock file if there is one
+if [ -f $DIR/*.lock ]
+then
+  rm *.lock
+fi
+
+# if there already is an index file in the directory just update everything
+if [ -f $DIR/index.html ]
+  then
+    httrack --update
+fi
 
 # download hacktricks
-httrack https://book.hacktricks.xyz/ -O ~/websites
+hacktricks=https://book.hacktricks.xyz/
+if [ ! -f /home/kali/websites/book.hacktricks.xyz ]
+  then
+  httrack $hacktricks -O ~/websites
+fi
+
 # download GTFO bins
-# httrack https://gtfobins.github.io/ -O ~/websites
+gtfobins=https://gtfobins.github.io/
+if [ ! -f /home/kali/websites/gtfobins.github.io ]
+  then
+  httrack $gtfobins -O ~/websites
+fi
 
+# download pentest monkey reverse shell cheat sheets
+pentestmonkey=https://pentestmonkey.net
+if [ ! -f /home/kali/websites/pentestmonkey.net ]
+  then
+  httrack $pentestmonkey -O ~/websites
+fi
 
+echo "Websites are now mirrored to $DIR.\nTo visit:\ncd ~/websites\npython3 -m http.server\nvisit http://localhost:8000/<nameofmirroredwebsitehere>"
 
